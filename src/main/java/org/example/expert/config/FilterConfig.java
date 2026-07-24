e20593a74ce137617b6c86c935bf5193e654765e
choiyongbum <72883647+choiyongbum@users.noreply.github.com>
2026-07-25 00:56:27 +0900

    Lv1: AuthUserArgumentResolver 정상 동작하도록 수정

--- src/main/java/org/example/expert/config/FilterConfig.java
+++ src/main/java/org/example/expert/config/FilterConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @RequiredArgsConstructor
