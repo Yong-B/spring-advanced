dc4ab4723abc813e847d5321ca37014fdc36ec9c
choiyongbum <72883647+choiyongbum@users.noreply.github.com>
2026-07-27 12:00:36 +0900

    feat : Lv 5.  API 로깅

---
+++ src/main/java/org/example/expert/config/AdminApiLoggingAspect.java
@@ -0,0 +1,50 @@
+﻿package org.example.expert.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class AdminApiLoggingAspect {
+
+    private final ObjectMapper objectMapper;
+
+    @Pointcut("execution(* org.example.expert.domain.comment.controller.CommentAdminController.deleteComment(..))")
+    private void commentAdminApi() {}
+
+    @Pointcut("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole(..))")
+    private void userAdminApi() {}
+
+    @Around("commentAdminApi() || userAdminApi()")
+    public Object logAdminApi(ProceedingJoinPoint joinPoint) throws Throwable {
+        HttpServletRequest request =
+                ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+
+        Long userId = (Long) request.getAttribute("userId");
+        String url = request.getRequestURI();
+        String requestBody = objectMapper.writeValueAsString(joinPoint.getArgs());
+
+        log.info("[ADMIN API 요청] userId={}, 시각={}, URL={}, requestBody={}",
+                userId, LocalDateTime.now(), url, requestBody);
+
+        Object response = joinPoint.proceed();
+
+        String responseBody = objectMapper.writeValueAsString(response);
+        log.info("[ADMIN API 응답] userId={}, URL={}, responseBody={}", userId, url, responseBody);
+
+        return response;
+    }
+}
